0c34cb3139980c17d7de7a05bc902c92d53d84f4
mdsohag65 <sohag92ahmed@gmail.com>
2022-07-17 14:56:42 +0600

    manage  services

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+import MangeServices from './Pages/ManageServices/MangeServices';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -28,6 +29,9 @@ function App() {
         </RequireAuth>}></Route>
         <Route path='addservice' element={<RequireAuth>
           <AddService />
+        </RequireAuth>}></Route>
+        <Route path='manage' element={<RequireAuth>
+          <MangeServices />
         </RequireAuth>}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
